b7f4a6c51fcd730857130bfbe6c7735c91b0ba35
Chris <lo.chris@live.ca>
2018-12-02 17:15:01 -0500

    Add files via upload
    pretty nice stuff for multiple forecasts

---
+++ bike_sales_sweep_forecast.R
@@ -0,0 +1,98 @@
+library(forecast)
+library(timetk)
+library(tidyverse)
+library(sweep)
+library(tidyquant)
+
+
+bike_sales <- as.tibble(bike_sales)
+
+mth_bike_sales <-
+  bike_sales %>% 
+  mutate(month = month(order.date)) %>% 
+  mutate(year = year(order.date)) %>% 
+  group_by(month,year) %>% 
+  summarise(total = sum(quantity))
+
+ggplot(data = mth_bike_sales, mapping = aes(x = month, y = total, group = year)) +
+  geom_area(position = 'stack', aes(fill = year))
+
+#View(bike_sales)
+
+monthly_qty_by_cat2 <- bike_sales %>%
+  mutate(order.month = as_date(as.yearmon(order.date))) %>%
+  group_by(category.secondary, order.month) %>%
+  summarise(total.qty = sum(quantity))
+monthly_qty_by_cat2
+
+monthly_qty_by_cat2_nest <- 
+  monthly_qty_by_cat2 %>% 
+  group_by(category.secondary) %>%
+  nest(.key = 'data.tbl')
+  
+monthly_qty_by_cat2_nest
+
+monthly_qty_by_cat2_ts <- 
+  monthly_qty_by_cat2_nest %>% 
+  mutate(data.ts = map(.x = data.tbl,
+                       .f = tk_ts,
+                       select = -order.month,
+                       freq = 12,
+                       start = 2011))
+monthly_qty_by_cat2_fit <- 
+  monthly_qty_by_cat2_ts %>% 
+  #map(vector, function)
+  mutate(fit.ets = map(data.ts, ets))
+
+monthly_qty_by_cat2_fit %>%
+  mutate(tidy = map(fit.ets, sw_tidy)) %>%
+  unnest(tidy, .drop = TRUE) %>%
+  spread(key = category.secondary, value = estimate)
+
+monthly_qty_by_cat2_fit %>%
+  mutate(glance = map(fit.ets, sw_glance)) %>%
+  unnest(glance, .drop = TRUE)
+
+#show the ets forecasts residuals
+augment_fit_ets <- monthly_qty_by_cat2_fit %>%
+  mutate(augment = map(fit.ets, sw_augment, timetk_idx = TRUE, rename_index = "date")) %>%
+  unnest(augment, .drop = TRUE)
+
+augment_fit_ets
+
+augment_fit_ets %>%
+  ggplot(aes(x = date, y = .resid, group = category.secondary)) +
+  geom_hline(yintercept = 0, color = "grey40") +
+  geom_line(color =palette_light()[[2]]) +
+  geom_smooth(method = "loess") +
+  labs(title = "Bike Quantity Sold By Secondary Category",
+       subtitle = "ETS Model Residuals", x = "") + 
+  theme_tq() +
+  facet_wrap(~ category.secondary, scale = "free_y", ncol = 3) +
+  scale_x_date(date_labels = "%Y")
+
+monthly_qty_by_cat2_fcast <- 
+  monthly_qty_by_cat2_fit %>% 
+  mutate(fcast.ets = map(fit.ets,forecast, h = 12))
+
+monthly_qty_by_cat2_fcast_tidy <- monthly_qty_by_cat2_fcast %>%
+  mutate(sweep = map(fcast.ets, sw_sweep, fitted = FALSE, timetk_idx = TRUE)) %>%
+  unnest(sweep)
+monthly_qty_by_cat2_fcast_tidy
+
+monthly_qty_by_cat2_fcast_tidy %>%
+  ggplot(aes(x = index, y = total.qty, color = key, group = category.secondary)) +
+  geom_ribbon(aes(ymin = lo.95, ymax = hi.95), 
+              fill = "#D5DBFF", color = NA, size = 0) +
+  geom_ribbon(aes(ymin = lo.80, ymax = hi.80, fill = key), 
+              fill = "#596DD5", color = NA, size = 0, alpha = 0.8) +
+  geom_line() +
+  labs(title = "Bike Quantity Sold By Secondary Category",
+       subtitle = "ETS Model Forecasts",
+       x = "", y = "Units") +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_color_tq() +
+  scale_fill_tq() +
+  facet_wrap(~ category.secondary, scales = "free_y", ncol = 3) +
+  theme_tq() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
